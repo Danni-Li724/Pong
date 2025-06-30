4cf9c7a9a690b3e6c73a88bc23f6afa0e22d95f1
Kwazzi724 <dannyli724@gmail.com>
2025-06-30 16:16:36 +1000

    my first editor scipt :D

---
+++ Assets/Editor/GameManagerFunctionCaller.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NetworkGameManager))]
+public class GameManagerFunctionCaller : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        NetworkGameManager networkGameManager = (NetworkGameManager)target;
+        // another script, maybe visual events? need to attach on same GO
+        VisualEventsManager visualEventsManager = networkGameManager.GetComponent<VisualEventsManager>();
+        
+        EditorGUILayout.Space(); // space between functions
+        EditorGUILayout.LabelField("Custom Actions", EditorStyles.boldLabel);
+        if (networkGameManager != null)
+        {
+            if (GUILayout.Button("Spaceship Mode"))
+            {
+                networkGameManager.StartSpaceshipModeClientRpc();
+            }
+        }
+        if (visualEventsManager != null)
+        {
+            if (GUILayout.Button("function name"))
+            {
+                // visual events function
+            }
+        }
+
+    }
+    
+}
